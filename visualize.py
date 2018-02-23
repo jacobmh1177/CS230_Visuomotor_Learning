60630a3ea19b781c5594dddd135e8e6e99f86ad1
jacobmh1177 <jacobmh@stanford.edu>
2018-02-23 00:46:45 -0800

    Add error visualization script

---
+++ visualize.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import utils
+import matplotlib.pyplot as plt
+
+def plot_results(params, data):
+    position_error = data["position error"]
+    pose_error = data["pose error"]
+    total_error = data["loss"]
+    plt.plot(position_error, label="position error")
+    plt.plot(pose_error, label="pose error")
+    plt.plot(total_error, label="total error")
+    plt.xlabel("# Epoch")
+    plt.ylabel("RMSE")
+    plt.legend()
+    plt.imsave(params.viz_output_file)
+
+if __name__ == "__main__":
+    params = utils.Params("./params.json")
+    results = pd.read_csv(params.viz_file)
+    plot_results(results)
